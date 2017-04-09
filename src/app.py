5492e74c103b3bc2156ca9f01ff0df92435b5d25
ChaseLEngel <chaselengel@gmail.com>
2017-04-09 02:57:14 -0700

    Moved navbar to function.

--- src/app.py
+++ src/app.py
@@ -7,6 +7,26 @@ from download_img import *
 
 LARGE_FONT= ("Verdana", 12)
 
+def navbar(frame, controller, current_page):
+    #start button
+    button1 = tk.Button(frame, text="Start",
+        command=lambda: controller.show_frame(StartPage))
+    button1.grid(row=0, column=0)
+    #options button
+    button2 = tk.Button(frame, text="Options",
+        command=lambda: controller.show_frame(OptionsPage))
+    button2.grid(row=0, column=1)
+    #commit button
+    button3 = tk.Button(frame, text="Commit",
+        command=lambda: controller.show_frame(CommitPage))
+    button3.grid(row=0, column=2)
+    #backup button
+    button4 = tk.Button(frame, text="Backup",
+        command=lambda: controller.show_frame(BackupPage))
+    button4.grid(row=0, column=3)
+    label = tk.Label(frame, text=current_page, font=LARGE_FONT)
+    label.grid(row=0, column=4)
+
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         data = PersistentData()
@@ -34,24 +54,8 @@ class StartPage(tk.Frame):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
         f = tk.Frame(self)
-        #start button
-        button1 = tk.Button(f, text="Start",
-                command=lambda: controller.show_frame(StartPage))
-        button1.grid(row=0, column=0)
-        #options button
-        button2 = tk.Button(f, text="Options",
-                command=lambda: controller.show_frame(OptionsPage))
-        button2.grid(row=0, column=1)
-        #commit button
-        button3 = tk.Button(f, text="Commit",
-                command=lambda: controller.show_frame(CommitPage))
-        button3.grid(row=0, column=2)
-        #backup button
-        button4 = tk.Button(f, text="Backup",
-                command=lambda: controller.show_frame(BackupPage))
-        button4.grid(row=0, column=3)
-        label = tk.Label(f, text="Start Page", font=LARGE_FONT)
-        label.grid(row=0, column=4)
+
+        navbar(f, controller, "Start Page")
 
         ISO_Entry = make_entry(f, "select iso")
         data.setISOFile(ISO_Entry.get())
@@ -75,86 +79,52 @@ class OptionsPage(tk.Frame):
         self.canvas.create_window((4,4), window=self.frame, anchor="nw")
         self.frame.bind("<Configure>", self.onFrameConfigure)
 
-        self.navbar()
+        navbar(self.frame, self.controller, "Options Page")
         self.populate()
 
     def onFrameConfigure(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-    def navbar(self):
-       #start button
-       button1 = tk.Button(self.frame, text="Start",
-               command=lambda: self.controller.show_frame(StartPage))
-       button1.grid(row=0, column=0)
-       #options button
-       button2 = tk.Button(self.frame, text="Options",
-               command=lambda: self.controller.show_frame(OptionsPage))
-       button2.grid(row=0, column=1)
-       #commit button
-       button3 = tk.Button(self.frame, text="Commit",
-               command=lambda: self.controller.show_frame(CommitPage))
-       button3.grid(row=0, column=2)
-       #backup button
-       button4 = tk.Button(self.frame, text="Backup",
-               command=lambda: self.controller.show_frame(BackupPage))
-       button4.grid(row=0, column=3)
-       label = tk.Label(self.frame, text="Options Page", font=LARGE_FONT)
-       label.grid(row=0, column=4)
 
     # Add all settings to grid
     def populate(self):
-       settings = [
-           {"name": "hdmi_safe"},
-           {"name": "disable_overscan"},
-           {"name": "overscan_left"},
-           {"name": "overscan_right"},
-           {"name": "overscan_top"},
-           {"name": "overscan_bottom"},
-           {"name": "framebuffer_width"},
-           {"name": "framebuffer_height"},
-           {"name": "hdmi_force_hotplug"},
-           {"name": "hdmi_group"},
-           {"name": "hdmi_mode"},
-           {"name": "hdmi_drive"},
-           {"name": "config_hdmi_boost"},
-           {"name": "sdtv_mode"},
-           {"name": "arm_freq"},
-           {"name": "i2c"},
-           {"name": "i2s"},
-           {"name": "spi"},
-           {"name": "lirc-rpi"},
-           {"name": "audio"}
-       ]
-
-       current_row = 1
-       for setting in settings:
+        settings = [
+            {"name": "hdmi_safe"},
+            {"name": "disable_overscan"},
+            {"name": "overscan_left"},
+            {"name": "overscan_right"},
+            {"name": "overscan_top"},
+            {"name": "overscan_bottom"},
+            {"name": "framebuffer_width"},
+            {"name": "framebuffer_height"},
+            {"name": "hdmi_force_hotplug"},
+            {"name": "hdmi_group"},
+            {"name": "hdmi_mode"},
+            {"name": "hdmi_drive"},
+            {"name": "config_hdmi_boost"},
+            {"name": "sdtv_mode"},
+            {"name": "arm_freq"},
+            {"name": "i2c"},
+            {"name": "i2s"},
+            {"name": "spi"},
+            {"name": "lirc-rpi"},
+            {"name": "audio"}
+        ]
+        current_row = 1
+        for setting in settings:
            tk.Label(self.frame, text=setting["name"]).grid(row=current_row, column=0)
            make_entry(self.frame, "").grid(row=current_row, column=1)
            current_row = current_row + 1
+        button1 = tk.Button(self.frame, text="Next",
+            command=lambda: self.controller.show_frame(CommitPage))
+        button1.grid(row=current_row, column=4)
 
 class CommitPage(tk.Frame):
     def __init__(self, parent, controller):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
         f = tk.Frame(self)
-        #start button
-        button1 = tk.Button(f, text="Start",
-                command=lambda: controller.show_frame(StartPage))
-        button1.grid(row=0, column=0)
-        #options button
-        button2 = tk.Button(f, text="Options",
-                command=lambda: controller.show_frame(OptionsPage))
-        button2.grid(row=0, column=1)
-        #commit button
-        button3 = tk.Button(f, text="Commit",
-                command=lambda: controller.show_frame(CommitPage))
-        button3.grid(row=0, column=2)
-        #backup button
-        button4 = tk.Button(f, text="Backup",
-                command=lambda: controller.show_frame(BackupPage))
-        button4.grid(row=0, column=3)
-        label = tk.Label(f, text="Commit Page", font=LARGE_FONT)
-        label.grid(row=0, column=4)
+        navbar(f, controller, "Commit Page")
         f.pack(fill=tk.BOTH, expand=1)
 
 class BackupPage(tk.Frame):
@@ -162,24 +132,7 @@ class BackupPage(tk.Frame):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
         f = tk.Frame(self)
-        #start button
-        button1 = tk.Button(f, text="Start",
-                command=lambda: controller.show_frame(StartPage))
-        button1.grid(row=0, column=0)
-        #options button
-        button2 = tk.Button(f, text="Options",
-                command=lambda: controller.show_frame(OptionsPage))
-        button2.grid(row=0, column=1)
-        #commit button
-        button3 = tk.Button(f, text="Commit",
-                command=lambda: controller.show_frame(CommitPage))
-        button3.grid(row=0, column=2)
-        #backup button
-        button4 = tk.Button(f, text="Backup",
-                command=lambda: controller.show_frame(BackupPage))
-        button4.grid(row=0, column=3)
-        label = tk.Label(f, text="Backup Page", font=LARGE_FONT)
-        label.grid(row=0, column=4)
+        navbar(f, controller, "Backup Page")
         f.pack(fill=tk.BOTH, expand=1)
 
 
