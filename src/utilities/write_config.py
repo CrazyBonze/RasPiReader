b65fdd41a4dab694d9cb63da474eecb3bfc039e8
ChaseLEngel <chaselengel@gmail.com>
2017-04-08 22:52:10 -0700

    Moved files to utilities. Added function to write settings to config.txt.

---
+++ src/utilities/write_config.py
@@ -0,0 +1,36 @@
+import os
+
+# Takes a dictionary of settings and their values 
+# and writes them to sdcard/boot/config.txt
+def write(sdcard, options):
+    config_path = os.path.join(sdcard, "boot/config.txt")
+    config_file = open(config_path, 'w')
+    settings = [
+            'hdmi_safe',
+            'disable_overscan',
+            'overscan_left',
+            'overscan_right',
+            'overscan_top',
+            'overscan_bottom',
+            'framebuffer_width',
+            'framebuffer_height',
+            'hdmi_force_hotplug',
+            'hdmi_group',
+            'hdmi_mode',
+            'hdmi_drive',
+            'config_hdmi_boost',
+            'sdtv_mode',
+            'arm_freq',
+            'i2c_arm',
+            'i2s',
+            'spi',
+            'lirc-rpi',
+            'audio'
+            ]
+    for setting in settings:
+        if setting in options:
+            config_file.write("{0}={1}\n".format(setting, options[setting]))
+    config_file.close()
+
+if __name__ == '__main__':
+    write('../../../testing/', {'hdmi_safe': '1', 'sdtv_mode': '2'})
