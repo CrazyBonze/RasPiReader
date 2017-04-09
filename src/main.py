5a62274cd9922497179ef1b0df43507a81ee8f58
Michael Taylor <90michaeltaylor@gmail.com>
2017-04-09 09:03:47 -0700

    We now demand root.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3.5
 
 from app import App
+import os
 
 def main():
+    if os.geteuid() != 0:
+        exit("Needs root permissions to berform dd operations.")
     app = App()
     app.mainloop()
+
 
 if __name__ == '__main__':
     main()
