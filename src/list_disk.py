f14d6f1c1c40558868ed1ee31cb34a737eca7304
ChaseLEngel <chaselengel@gmail.com>
2017-04-09 08:57:51 -0700

    Added list disk for darwin.

--- src/list_disk.py
+++ src/list_disk.py
@@ -3,13 +3,14 @@ import subprocess, os, platform, itertools
 def list_disks():
     system_name = platform.platform()
     if system_name.startswith("Darwin"):
-        return []
-        #chase will have to do this
-        #disk_command = ["diskutil", "info", "-all"]
-        #p = subprocess.Popen(disk_command, stdout=subprocess.PIPE)
-        #output, err = p.communicate()
-        #Do regex stuff
-        # ...
+        p1 = subprocess.Popen(["diskutil", "list"], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["grep", "/dev"], stdin=p1.stdout, stdout=subprocess.PIPE)
+        p3 = subprocess.Popen(["awk", "{print $1}"], stdin=p2.stdout, stdout=subprocess.PIPE)
+        p1.stdout.close()
+        p2.stdout.close()
+        output, err = p3.communicate()
+        return output.decode('utf-8').split()
+
     elif system_name.startswith("Linux"):
         disk_command = ["lsblk", "-n", "-o", "KNAME,RM"]
         p = subprocess.Popen(disk_command, stdout=subprocess.PIPE)
