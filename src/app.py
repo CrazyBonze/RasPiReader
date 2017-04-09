a179b349e2601e8b33f11eba77eb56879ccdb44a
Michael Taylor <90michaeltaylor@gmail.com>
2017-04-09 01:42:32 -0700

    New way that can handle grids.

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from persistent_data import PersistentData
 from wifi import *
+from download_img import *
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -32,28 +33,34 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
+        f = tk.Frame(self)
         #start button
-        button1 = tk.Button(self, text="Start",
+        button1 = tk.Button(f, text="Start",
                 command=lambda: controller.show_frame(StartPage))
-        button1.pack(side=tk.LEFT, anchor=tk.N)
+        button1.grid(row=0, column=0)
         #options button
-        button2 = tk.Button(self, text="Options",
+        button2 = tk.Button(f, text="Options",
                 command=lambda: controller.show_frame(OptionsPage))
-        button2.pack(side=tk.LEFT, anchor=tk.N)
+        button2.grid(row=0, column=1)
         #commit button
-        button3 = tk.Button(self, text="Commit",
+        button3 = tk.Button(f, text="Commit",
                 command=lambda: controller.show_frame(CommitPage))
-        button3.pack(side=tk.LEFT, anchor=tk.N)
+        button3.grid(row=0, column=2)
         #backup button
-        button4 = tk.Button(self, text="Backup",
+        button4 = tk.Button(f, text="Backup",
                 command=lambda: controller.show_frame(BackupPage))
-        button4.pack(side=tk.LEFT, anchor=tk.N)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(anchor=tk.N)
+        button4.grid(row=0, column=3)
+        label = tk.Label(f, text="Start Page", font=LARGE_FONT)
+        label.grid(row=0, column=4)
 
-        ISO_Entry = make_entry(self, "select iso")
-        ISO_Entry.pack(anchor=tk.W)
+        ISO_Entry = make_entry(f, "select iso")
         data.setISOFile(ISO_Entry.get())
+        ISO_Entry.grid(row=2, column=0)
+
+        menu = make_menu(f, image_list())
+        f.pack(fill=tk.BOTH, expand=1)
+
+
 
 
 class OptionsPage(tk.Frame):
@@ -62,35 +69,38 @@ class OptionsPage(tk.Frame):
         #print(data.getISOFile())
         tk.Frame.__init__(self, parent)
         #start button
-        button1 = tk.Button(self, text="Start",
+        f = tk.Frame(self)
+        button1 = tk.Button(f, text="Start",
                 command=lambda: controller.show_frame(StartPage))
-        button1.pack(side=tk.LEFT, anchor=tk.N)
+        button1.grid(row=0, column=0)
         #options button
-        button2 = tk.Button(self, text="Options",
+        button2 = tk.Button(f, text="Options",
                 command=lambda: controller.show_frame(OptionsPage))
-        button2.pack(side=tk.LEFT, anchor=tk.N)
+        button2.grid(row=0, column=1)
         #commit button
-        button3 = tk.Button(self, text="Commit",
+        button3 = tk.Button(f, text="Commit",
                 command=lambda: controller.show_frame(CommitPage))
-        button3.pack(side=tk.LEFT, anchor=tk.N)
+        button3.grid(row=0, column=2)
         #backup button
-        button4 = tk.Button(self, text="Backup",
+        button4 = tk.Button(f, text="Backup",
                 command=lambda: controller.show_frame(BackupPage))
-        button4.pack(side=tk.LEFT, anchor=tk.N)
-        label = tk.Label(self, text="Options Page", font=LARGE_FONT)
-        label.pack(anchor=tk.N)
+        button4.grid(row=0, column=3)
+        label = tk.Label(f, text="Options Page", font=LARGE_FONT)
+        label.grid(row=0, column=4)
 
-        ISO_Menue = make_menu(self, ssid_scan())
-        ISO_Menue.pack()
+        #ISO_Menue = make_menu(self, ssid_scan())
+        #ISO_Menue.pack()
 
-        ISO_Entry = make_entry(self, data.getISOFile())
-        ISO_Entry.pack(anchor=tk.W)
-        data.setISOFile(ISO_Entry.get())
+        #ISO_Entry = make_entry(self, data.getISOFile())
+        #ISO_Entry.pack(anchor=tk.W)
+        #data.setISOFile(ISO_Entry.get())
+        f.pack(fill=tk.BOTH, expand=1)
 
 class CommitPage(tk.Frame):
     def __init__(self, parent, controller):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
+        f = tk.Frame(self)
         #start button
         button1 = tk.Button(self, text="Start",
                 command=lambda: controller.show_frame(StartPage))
@@ -109,11 +119,13 @@ class CommitPage(tk.Frame):
         button4.pack(side=tk.LEFT, anchor=tk.N)
         label = tk.Label(self, text="Commit Page", font=LARGE_FONT)
         label.pack(anchor=tk.N)
+        f.pack(fill=tk.BOTH, expand=1)
 
 class BackupPage(tk.Frame):
     def __init__(self, parent, controller):
         data = PersistentData()
         tk.Frame.__init__(self, parent)
+        f = tk.Frame(self)
         #start button
         button1 = tk.Button(self, text="Start",
                 command=lambda: controller.show_frame(StartPage))
@@ -132,6 +144,7 @@ class BackupPage(tk.Frame):
         button4.pack(side=tk.LEFT, anchor=tk.N)
         label = tk.Label(self, text="Backup Page", font=LARGE_FONT)
         label.pack(anchor=tk.N)
+        f.pack(fill=tk.BOTH, expand=1)
 
 
 def make_entry(parent, caption, width=None, **options):
