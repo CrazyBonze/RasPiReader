6d07eb6c55a4b3c24b98b86097f980f4f89c5e16
Michael Taylor <90michaeltaylor@gmail.com>
2017-04-08 17:36:11 -0700

    Added top row buttons.

--- src/app.py
+++ src/app.py
@@ -7,9 +7,40 @@ class App(Frame):
         Frame.__init__(self, parent)
         self.parent = parent
         self.initUI()
+        #Button().grid(row=self.row, column=self.column)
+        self.buttons = [
+                { 'name':'Start',   'function':self.Start },
+                { 'name':'Options', 'function':self.Options },
+                { 'name':'Commit',  'function':self.Commit },
+                { 'name':'Backup',  'function':self.Backup }
+                ]
+        self.BuildButtons(self.buttons)
+
+    def BuildButtons(self, buttons):
+        pos = 0
+        for b in self.buttons:
+            label = b['name']
+            y = Button(self.parent, text=b['name'], command=b['function'])
+            y.grid(row=3, column=2+pos)
+            pos = pos + 2
+
+
+    def Start(self):
+        print("hello from start.")
+
+    def Options(self):
+        print("hello from boot loader")
+
+    def Commit(self):
+        print("hello from commit")
+
+    def Backup(self):
+        print("hello from Backup")
+
+
 
     def initUI(self):
-        self.parent.title("hello")
-        self.pack(fill=BOTH, expand=1)
+        self.parent.title("RasPiReader")
+        #self.pack(fill=BOTH, expand=1)
         canvas = Canvas(self)
 
